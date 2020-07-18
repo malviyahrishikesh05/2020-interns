518a9d803e83e2b04c6b732481768374ab14a543
malviyahrishikesh05 <66720763+malviyahrishikesh05@users.noreply.github.com>
2020-07-18 15:33:05 +0530

    task 1
    Task 1 is completed . Currently no features is added in this task.

---
+++ task1.py
@@ -0,0 +1,43 @@
+#impoting json library to read json file
+import json 
+#importing matplotlib to plot graph
+import matplotlib.pyplot as plt  
+# Opening JSON file 
+f = open('data.json',) 
+  
+# returns JSON object as a dictionary 
+data = json.load(f)
+#list of INR from 2019-01-01 to 2019-01-31 
+INR_list_1_31_jan=[]
+# Iterating through the json 
+# COnverting Data Of INR by date to list 
+for i in range (1,31):
+  if i <10:#for date 1 to 9
+    date="2019-01-0"+str(i)
+    if date in data["rates"]:# to check wether date is in the file if not the puting the data of previous date 
+      INR_list_1_31_jan.append(data["rates"][str(date)]["INR"])
+    else:
+      if len(INR_list_1_31_jan)>0:
+        INR_list_1_31_jan.append(INR_list_1_31_jan[len(INR_list_1_31_jan)-1])
+      
+  else:
+    date="2019-01-"+str(i)
+    if date in data["rates"]:# to check wether date is in the file if not the puting the data of previous date
+      INR_list_1_31_jan.append(data["rates"][str(date)]["INR"])
+    else:
+      if len(INR_list_1_31_jan)>0:
+        INR_list_1_31_jan.append(INR_list_1_31_jan[len(INR_list_1_31_jan)-1])
+
+ 
+#ploting the point
+plt.plot(INR_list_1_31_jan,label="INR")
+# naming the x axis 
+plt.xlabel('1 Jan 2019 to 31 Jan 2019') 
+# naming the y axis 
+plt.ylabel('INR exchange rate against EUR ') 
+  
+# giving a title to my graph 
+plt.title('My first graph!') 
+  
+# function to show the plot 
+plt.show()
